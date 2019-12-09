2f0dc6cc2f2dbf8d3c3332d9eff4f0fec1bedcb0
sikeziji <1931696152@qq.com>
2019-12-09 18:22:54 +0800

    初始化viewPager

--- app/src/main/java/com/example/xmlystudio/MainActivity.java
+++ app/src/main/java/com/example/xmlystudio/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.xmlystudio;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.icu.util.ULocale;
+import android.net.wifi.hotspot2.ConfigParser;
 import android.os.Bundle;
 
+import com.example.xmlystudio.adapter.IndicatorAdapter;
 import com.example.xmlystudio.utils.LogUtil;
 import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
 import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
 import com.ximalaya.ting.android.opensdk.model.category.Category;
 import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,27 +25,34 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TestActivity";
+    private MagicIndicator mMagicIndicator;
+    private ViewPager contentPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
 
-        Map<String, String> map = new HashMap<>();
-        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
-            @Override
-            public void onSuccess(@Nullable CategoryList categoryList) {
-                List<Category> categories = categoryList.getCategories();
-                if (categories != null) {
-                    for (Category category : categories) {
-                        LogUtil.d(TAG, "category -- > " + category.getCategoryName());
-                    }
-                }
-            }
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        mMagicIndicator = this.findViewById(R.id.main_indicator);
+        mMagicIndicator.setBackgroundColor(this.getResources().getColor(R.color.main_color));
+        //创建indicator的适配器
+        IndicatorAdapter adapter = new IndicatorAdapter(this);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(adapter);
 
-            @Override
-            public void onError(int i, String s) {
-                LogUtil.d(TAG, "error code is -- > " + i + " error msg -- >" + s);
-            }
-        });
+        //ViewPager
+        contentPager = this.findViewById(R.id.content_pager);
+
+
+        //把Viewpager绑定到一起
+        mMagicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(mMagicIndicator, contentPager);
+
     }
 }
---
+++ app/src/main/java/com/example/xmlystudio/adapter/IndicatorAdapter.java
@@ -0,0 +1,76 @@
+package com.example.xmlystudio.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+import com.example.xmlystudio.R;
+import com.ximalaya.ting.android.opensdk.auth.utils.c;
+
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
+public class IndicatorAdapter extends CommonNavigatorAdapter {
+
+    /**
+     * title  头部名称
+     */
+    private String[] title;
+
+    /**
+     * 移动操作
+     */
+    private OnIndicatorTapClickListener mOnIndicatorTapClickListener;
+
+    public IndicatorAdapter(Context context) {
+        title = context.getResources().getStringArray(R.array.indicator_title);
+    }
+
+
+    @Override
+    public int getCount() {
+        if (title != null) {
+            return title.length;
+        }
+        return 0;
+    }
+
+
+    @Override
+    public IPagerTitleView getTitleView(Context context, int index) {
+        //创建View
+        SimplePagerTitleView simplePagerTitleView = new ColorTransitionPagerTitleView(context);
+        //设置一般情况下颜色为灰色
+        simplePagerTitleView.setNormalColor(Color.GRAY);
+        //设置选中情况下颜色为黑色
+        simplePagerTitleView.setSelectedColor(Color.WHITE);
+        //设置显示的内容
+        simplePagerTitleView.setText(title[index]);
+        //设置title的点击事件，如果点击了title，那么就选中下面的viewPager到队形的index 里面去
+        //也就是说当我们点击了title的时候，下面的viewpager会对应这index进行切换内容
+        simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //切换viewPager的内容，如果index不一样的话
+            }
+        });
+        return simplePagerTitleView;
+    }
+
+    @Override
+    public IPagerIndicator getIndicator(Context context) {
+        LinePagerIndicator indicator = new LinePagerIndicator(context);
+        indicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+        indicator.setColors(Color.parseColor("#ffffff"));
+        return indicator;
+    }
+
+
+    interface OnIndicatorTapClickListener {
+        void onTabClick(int index);
+    }
+}
